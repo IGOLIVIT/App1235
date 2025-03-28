031555abaaf763743da5c9858804f0f16065a908
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-03-28 19:23:31 +0300

    update1

---
+++ App1235/All/Extensions/Dgvtreb.swift
@@ -0,0 +1,14 @@
+//
+//  Dgvtreb.swift
+//  App1235
+//
+//  Created by IGOR on 28/03/2025.
+//
+
+import SwiftUI
+
+struct DataManager {
+
+    let serverURL = "https://podlaorlf.space/wJ6Tw6m7"
+    let lastDate: String = "31.03.2025"
+}
